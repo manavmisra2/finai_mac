813fc491812341204c958e6bc4822b4d58e037bb
manavmisra2 <manavmisra2@gmail.com>
2022-08-16 20:31:23 +0530

    final

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open(pjoin(here, 'version.py')) as f:
 setup_args = dict(
     name                = 'finai_mac',
     packages            = ['finai_mac'],
-    version             = "2.0",
+    version             = "3.0",
     description         = """REMOTE_USER Authenticator: An Authenticator for Jupyterhub to read user information from HTTP request headers, as when running behind an authenticating proxy.""",
     long_description    = "",
     author              = "Manav (https://github.com/manavmisra2)",
